2c9758d9c89e04ed644f21c9191be5835adc5531
boyboi86 <boyboi86@gmail.com>
2016-11-17 13:58:16 +0800

    added operators

--- String/src/com/String/Main.java
+++ String/src/com/String/Main.java
@@ -3,6 +3,12 @@ package com.String;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// String allows multiple chars and to define type, S in String has to be uppercase
+        String newString = "hahaha new string";
+        System.out.println(newString);
+//        Strings can be "stacked" to form sentences, rmb string needs double quotes
+        newString = newString + "hahahaha another new string";
+//        the String prints out has been used twice and stacked
+        System.out.println(newString);
     }
 }
